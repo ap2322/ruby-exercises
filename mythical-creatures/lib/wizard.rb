9c35d3cb4fb5dc12678de5df445e08cf79a052aa
Alice Post <ap2322@gmail.com>
2019-08-22 21:57:18 -0600

    wizard and test

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -1,6 +1,29 @@
 class Wizard
   attr_reader :name
-  def initialize(name)
+
+  def initialize(name, bearded: true, rested: true)
     @name = name
+    @bearded = bearded
+    @rested = rested
+    @spells_cast = 0
   end
+
+  def bearded?
+    @bearded
+  end
+
+  def incantation(spell)
+    "sudo #{spell}"
+  end
+
+  def rested?
+    @rested = false if @spells_cast >= 3
+    @rested
+  end
+
+  def cast
+    @spells_cast +=1
+    "MAGIC MISSILE"
+  end
+
 end
